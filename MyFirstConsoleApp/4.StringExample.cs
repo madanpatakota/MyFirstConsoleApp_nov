4c8f784a3bc05af2bad850de4a34ac605deddbb3
Madan Patakota <madan.patakota@gmail.com>
2025-11-07 10:07:50 +0530

    string

---
+++ MyFirstConsoleApp/4.StringExample.cs
@@ -0,0 +1,34 @@
+﻿using System;
+class StringExample
+{
+    static void Main()
+    {
+        string coursename = "Csharp";
+
+        Console.WriteLine(coursename);
+
+        string uCourseName = "\"Csharp\"";
+        Console.WriteLine(uCourseName);
+
+        string lStatus = "one. \n two. \n three";
+        Console.WriteLine(lStatus);
+
+        string myProjectPath = "D:\\Csharp_Nov\\MyFirstConsoleApp";
+        Console.WriteLine(myProjectPath);
+
+        string myProjectPath1 = @"D:\Csharp_Nov\MyFirstConsoleApp";
+        Console.WriteLine(myProjectPath1);
+
+        string sName = "Mery";
+        int age = 15;
+
+        //string interpolaiton technique
+        string studentDetails = $"Student name : {sName} , Student age : {age} ";
+        Console.WriteLine(studentDetails);
+
+        //${ }
+
+
+    }
+}
+
---
+++ MyFirstConsoleApp/5.StringMoreConcepts.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+
+class StringMoreConcepts
+{
+	static void Main()
+	{
+
+		string myProjectPath = "D:\\Csharp_Nov\\MyFirstConsoleApp";
+
+		//Members ---> Family   f , m , b , s , gf , gm
+		//myProjectPath.Length
+
+		string name1 = "john";   //4
+		int lengt = name1.Length;             //4
+		Console.WriteLine(lengt); //4
+
+		string name2 = "john";
+		string uName2  = name2.ToUpper(); // "JOHN"  // ?? what could be return data type ??
+		Console.WriteLine(uName2);
+
+		string name3 = "JohN";
+		string uName3 = name3.ToLower(); // "john"  // ?? what could be return data type ??
+		Console.WriteLine(uName3);
+
+
+
+	}
+}
+
