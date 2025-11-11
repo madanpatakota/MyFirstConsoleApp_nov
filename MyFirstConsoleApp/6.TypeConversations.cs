47ecbf138dc2055a35177a23ba13511c7d4dfde8
Madan Patakota <madan.patakota@gmail.com>
2025-11-11 10:21:02 +0530

    data

---
+++ MyFirstConsoleApp/6.TypeConversations.cs
@@ -0,0 +1,113 @@
+﻿using System;
+class TypeConversations
+{
+    static void Main()
+    {
+        //Type conversations are 2 types
+
+        ////implicit type conv 
+        ////numeric type int - byte  -- i do't get any kind of headaches
+        //byte age = 20;  // numeic
+        //int largeAge = age;  // i can do that
+
+        ////explicit type conv
+        //int salary = 10000;  //just assume emp salary
+        ////byte smallsalary = salary;
+        //u salary1 = salary;
+
+
+
+
+        //Explict   --
+
+
+        // you are going to give the guidance to the compiler exactly what is the type i.e.
+
+        // explicit type
+        int age = 25; //explicity you are telling -- compiler 
+        string name = "Csharp"; //
+        double package = 67890.90;
+
+        //Explicit type conversation
+        double price = 12356.75;
+        int amount   = (int)price;   //its very imporantve  you have to manually casting that.
+        Console.WriteLine(amount);  //12356   //you are loosing    .75(fractional) value...
+
+
+        long phoneNumber = 7878776668;
+        byte byte1 = (byte)phoneNumber;
+        Console.WriteLine(byte1);   //again 
+
+
+
+
+
+        //implict type 
+        // let your compiler understand what it the type you have assinged value.
+        var age1 = 25;  //complier is understanding
+        var name1 = "csharp"; //complier understand the string
+
+
+
+        //Happens automatically without loosing the data.
+        // here you do't need any cast
+
+        int number = 100;
+        double result = number; // int --> double
+        Console.WriteLine(result);   // 100
+
+
+        // byte - int
+
+        // int - long
+
+        // float - double
+
+
+
+        //boxing and unboxing 
+        // convert the value type to ref   type   -- Boxing
+        // convert the ref   type to value type   -- Unboxing
+
+
+        //value type -  10 , 20 , 123.78;  actaul type
+        //value type stores the actual value
+
+        //ref   type - Object(can store any value)  , string 
+        // ref type stores address of the actual value.
+
+
+
+
+        //Boxing : Exammple
+        int    RoomNo = 10;      // here roomno is the value type stores the actual value i.e. 10   
+        object obj1 = RoomNo;    // Generally object can stores the ref of the actual data
+                                 // Now obj1 take the ref of to the boxed objet(roomno)
+
+
+
+        object obj2 = 10;
+        int RoomNo2 = (int)obj2;   //unbox
+
+        string str1 = "Csharp";
+        string str2 = str1;     //now str2 pointing to same ref of str1
+
+
+        Console.WriteLine(str1);   //Csharp;
+        Console.WriteLine(str2);   //Csharp;
+
+        str2 = ".net";
+        Console.WriteLine(str2);   //.net;  //now str2 makes it point to the new string 
+
+
+        //string , array , class
+
+
+
+       
+
+        
+
+    }
+}
+
---
+++ MyFirstConsoleApp/7.Array.cs
@@ -0,0 +1,51 @@
+﻿using System;
+class Arrays
+{
+    static void Main()
+    {
+        // [10 , 20 , 30]
+
+        int[] numbersGroup = new int[2];  //numbersGroup accepts 2 values;
+        numbersGroup[0] = 11;
+        numbersGroup[1] = 21;
+
+        Console.WriteLine(numbersGroup[0]);  // 11
+        Console.WriteLine(numbersGroup[1]);  // 21
+
+        //numbersGroup[2] = 32;   //No Complie time error.
+        //Console.WriteLine(numbersGroup[2]);
+
+
+        //string length , array length
+
+        int[] numbersGroup1 = new int[] { 101, 201, 301, 401 };
+        Console.WriteLine(numbersGroup1[numbersGroup1.Length - 1]);  // improvement 
+
+
+        numbersGroup1[2] = 300;
+        Console.WriteLine(numbersGroup1[2]);
+
+
+        // student1 : hey print each and every value..
+
+        //Console.WriteLine(numbersGroup1[0]);   // 101
+        //Console.WriteLine(numbersGroup1[1]);   // 201
+        //Console.WriteLine(numbersGroup1[2]);   // 300
+        //Console.WriteLine(numbersGroup1[3]);   // 401
+
+
+        //foreach (int number in numbersGroup1) {  
+        //    Console.WriteLine(number);
+        //}
+
+
+        foreach (var number in numbersGroup1)
+        {
+            Console.WriteLine(number);
+        }
+
+
+
+    }
+}
+
