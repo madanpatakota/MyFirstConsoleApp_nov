f21bc02423c894d747e394c3e08ee1a63ac2c4e6
Madan Patakota <madan.patakota@gmail.com>
2025-11-13 09:04:19 +0530

    code

---
+++ MyFirstConsoleApp/10.ConditionalStatements.cs
@@ -0,0 +1,67 @@
+﻿using System;
+class conditionalStatements
+{
+    static void Main()
+    {
+        // operatos
+        // if statemnt else if statement  else statement 
+
+        bool result = (10 < 20);
+
+        string bankName = "SBI";
+
+        if(bankName == "HDFC")
+        {
+            Console.WriteLine("MY Password is MadanHDFC!1234");
+        }
+        else if(bankName == "Axis")
+        {
+            Console.WriteLine("MY Password is MadanAxi!1234");
+        }
+        else if (bankName == "SBI")
+        {
+            Console.WriteLine("MY Password is MadanSBI!1234");
+        }
+        else
+        {
+            Console.WriteLine("You do't have permission to see the password");
+        }
+
+
+
+
+
+        //Switch ..... case
+
+
+
+
+        //switch...case .. assume your swith board in your home...
+        // executes the staments based on the case
+
+
+        string switchKey = "";  // value
+
+        switch(switchKey)
+        {
+            //we will prepare the cases
+            case "MobilePhoneChargerKey":
+                Console.WriteLine("You switched phone charger");
+                break;
+            case "tvKey":
+                Console.WriteLine("You switched TV");
+                break;
+            case "fanKey":
+                Console.WriteLine("You switched the fan");
+                break;
+            default:
+                Console.WriteLine("No key is available");
+                break;
+        }
+
+
+
+
+    }
+}
+
---
+++ MyFirstConsoleApp/9.Operators.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+class ArthamaticOperators
+{
+    //Undestand the behaviour of the operator...
+
+    static void Main()
+    {
+        //Arthamatic opertos   + , - , * , / 9:03
+        int result = 10 + 20;  // 30 numeric
+        Console.WriteLine(result); // 30
+
+        int result1 = 20 - 10;  // 30 numeric
+        Console.WriteLine(result1); // 10
+
+        // == , != , < , > , <= , >=  // true or false 
+        bool isEqual = 10 == 10;
+        Console.WriteLine(isEqual); // true , 10 , "Madan"
+
+        bool isEqual1 = 10 == 20;
+        Console.WriteLine(isEqual1); // false , 10 , "Madan"
+
+        bool isEqual2 = 10 < 20;
+        Console.WriteLine(isEqual2); // false , 10 , "Madan"
+
+        bool isEqual3 = 10 > 20;
+        Console.WriteLine(isEqual3); // false , 10 , "Madan"
+
+        bool isEqual4 = (10 >= 20);
+        Console.WriteLine(isEqual4); // false , 10 , "Madan"
+
+        bool isEqual5 = (10 <= 20);
+        Console.WriteLine(isEqual5); // false , 10 , "Madan"
+
+
+
+        // (3+4) < 6
+
+        //&& , || , !
+
+        //T t - t  , t f - f , f t - f , f  f - f
+
+
+        Console.WriteLine("-----------------AND--------------");
+
+        bool isResult = (10 == 10) && (20 == 20);
+        Console.WriteLine(isResult);
+
+        bool isResult1 = (10 == 10) && (30 == 20);
+        Console.WriteLine(isResult1);
+
+        bool isResult2 = (20 == 10) && (20 == 20);
+        Console.WriteLine(isResult2);
+
+        bool isResult3 = (20 == 10) && (10 == 20);
+        Console.WriteLine(isResult3);
+
+
+
+
+        //T t - t  , t f - t , f t - t , f  f - f
+
+
+        Console.WriteLine("----------------OR----------------");
+
+        bool isResult4 = (10 == 10) || (20 == 20);
+        Console.WriteLine(isResult4);
+
+        bool isResult5 = (10 == 10) || (30 == 20);
+        Console.WriteLine(isResult5);
+
+        bool isResult6 = (20 == 10) || (20 == 20);
+        Console.WriteLine(isResult6);
+
+        bool isResult7 = (20 == 10) || (10 == 20);
+        Console.WriteLine(isResult7);
+
+
+        Console.WriteLine("----------------NOT  !----------------");
+
+        bool isResult8 = !(10 == 10);  //false
+        Console.WriteLine(isResult8);  
+
+        bool isResult9 = !(20 == 10);  //true
+        Console.WriteLine(isResult9);
+
+
+       // Ex : compare and Logical we will use in if else blocks
+
+
+    }
+}
+
